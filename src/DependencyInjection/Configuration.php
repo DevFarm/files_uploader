edf935b5de09c168a39b68927ec5f729f552539e
George <sumatorhak@gmail.com>
2019-10-23 17:28:13 +0300

    New skeleton

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DevFarm\FilesUploaderBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('files_uploader');
+        return $treeBuilder;
+    }
+}
---
+++ src/FilesUploaderBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DevFarm\FilesUploaderBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FilesUploaderBundle extends Bundle
+{
+}
